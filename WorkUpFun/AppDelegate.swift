2e33d8c103f8b6b9a3507c769048498e2ccc1027
ofEver <gaooof@hotmail.com>
2016-10-06 16:20:08 +0800

    Initial Commit

---
+++ WorkUpFun/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WorkUpFun
+//
+//  Created by apple on 16/10/6.
+//  Copyright © 2016年 Gaooof. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WorkUpFun/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WorkUpFun
+//
+//  Created by apple on 16/10/6.
+//  Copyright © 2016年 Gaooof. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
